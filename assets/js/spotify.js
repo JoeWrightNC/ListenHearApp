36d481696353494a7f3367e2b7a52d740d2fd90b
Joe Wright <joewrightart@gmail.com>
2018-04-18 10:07:40 -0400

    fingers crossed spotify now live for all users

---
+++ assets/js/spotify.js
@@ -0,0 +1,1 @@
+window.location.href = "https://accounts.spotify.com/authorize?client_id=<FromAppCreatedInSppotify>&response_type=token&redirect_uri=https://joewrightnc.github.io/indexReal.html&scope=user-read-private";
